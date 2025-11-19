1ce05d467e8de65255af5ecacc9f6cb7805284ce
virtualblues <danielv385@nycstudents.net>
2025-11-19 14:26:25 +0000

    did me some point stuff

--- java102/Main.java
+++ java102/Main.java
@@ -1,2 +1,19 @@
 package java102;
 
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(4, 3);
+        System.out.println("x: " + point.x + "y: " + point.y);
+        
+        Point xTranslation = point.translateX(3); // should be (7, 3)
+        Point yTranslation = point.translateY(-7); // should be (4, -4)
+        Point xyTranslation = point.translateX(-4).translateY(-3); // should be (0, 0)
+        System.out.println("x transl: " + xTranslation);
+        System.out.println("y transl: " + yTranslation);
+        System.out.println("xy transl: " + xyTranslation);
+        
+        Point a = new Point(1,1);
+        Point b = new Point(4,5);   
+        System.out.println("\npoint a: " + a + "\npoint b: " + b + " \ndistance between a and b: " + Point.distance(a, b));
+    }
+ }
--- java102/Point.java
+++ java102/Point.java
@@ -7,5 +7,22 @@ public class Point {
  public Point(double x, double y) {
   this.x = x;
   this.y = y;
+    }  
+
+ public Point translateX(double t) {
+  return new Point(x + t, y);
+    }
+
+ public Point translateY(double t) {
+  return new Point(x, y + t);
+    }
+ @Override
+ public String toString() {
+  return "(" + x + ", " + y + ")";
  }
+ 
+ public static double distance(Point p1, Point p2) {
+  return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+ }
+
 }
